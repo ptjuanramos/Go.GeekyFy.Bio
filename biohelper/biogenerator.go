ce5a82aef076bc9f0f5b4df1ee8d3a80275c5566
Juan Ramos <ptjuanramos@gmail.com>
2020-05-20 09:37:06 +0100

    Http client to make HTTP GET to a api to obtain random geek quotes

--- biohelper/biogenerator.go
+++ biohelper/biogenerator.go
@@ -1,7 +1,40 @@
 package biohelper
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+const quotesEndpoint = "https://geek-jokes.sameerkumar.website/api"
+const emptyString = ""
+
 //GetNewUserBio Get random bio string
 func GetNewUserBio() string {
-	//todo
-	return "Teste 2"
+	response, err := http.Get(quotesEndpoint)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return emptyString
+	}
+
+	if response.StatusCode == http.StatusOK {
+		randomQuote := getStringFromResponseBody(response.Body)
+		return randomQuote
+	}
+
+	return emptyString
+}
+
+func getStringFromResponseBody(body io.ReadCloser) string {
+	bodyAsBytes, err := ioutil.ReadAll(body)
+	body.Close()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return emptyString
+	}
+
+	return string(bodyAsBytes)
 }
